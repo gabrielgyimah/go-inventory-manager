58c9e47c54159decb97c4b53f93ce1616949366a
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2024-06-18 12:48:50 +0100

    Added a context for mapping our custom icon components name to the filenames

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,15 +6,18 @@ import { ThemeProvider } from '@/context/theme-context';
 import { AuthProvider } from '@/context/auth-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { AppIconProvider } from '@/context/app-icon-context';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <ThemeProvider>
         <GestureHandlerRootView>
-          <BottomSheetModalProvider>
-            <Slot />
-          </BottomSheetModalProvider>
+          <AppIconProvider>
+            <BottomSheetModalProvider>
+              <Slot />
+            </BottomSheetModalProvider>
+          </AppIconProvider>
         </GestureHandlerRootView>
       </ThemeProvider>
     </AuthProvider>

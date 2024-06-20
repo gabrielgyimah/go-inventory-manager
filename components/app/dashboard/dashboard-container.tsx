3ef8c04e4d5f5925196e162959d9b5e62ff8aa3d
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2024-06-20 13:13:31 +0100

    Started working on the dashboard

--- components/app/dashboard/dashboard-container.tsx
+++ components/app/dashboard/dashboard-container.tsx
@@ -1,20 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StyledPrimaryContainer } from '../ui/styled-components/style-container'
-import ThemeSettingsButton from '../settings/theme-settings-button'
-import AppIcons from '../ui/icons/app-icons'
+import { StyledH1MutedText, StyledH3MutedText, StyledH3PrimaryText } from '../ui/styled-components/style-texts'
+import { useAuth } from '@/context/auth-context'
+import { Ionicons } from '@expo/vector-icons'
+import { useTheme } from '@/context/theme-context'
+import CurrentTime from '../utils/time'
 
 export default function DashboardContainer() {
+  const { user } = useAuth()
+  const { theme } = useTheme()
   return (
-    <StyledPrimaryContainer style={{ justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Text>Dash board</Text>
-      <ThemeSettingsButton />
-      <AppIcons name='GreenEnvelope'/>
-      <AppIcons name='GreenNoProducts'/>
-      <AppIcons name='Televison'/>
-      <AppIcons name='Airpods'/>
-      <AppIcons name='Speakers'/>
-      <AppIcons name='Storage'/>
+    <StyledPrimaryContainer style={styles.container}>
+      <View style={styles.topContainer}>
+        <StyledH3PrimaryText text={`${user?.first_name} ${user?.last_name}`} />
+        <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+          <Ionicons name='location-outline' color={theme.text.muted} size={20}/>
+          <StyledH3MutedText style={{ fontWeight: 'normal'}} text=' | '/>
+          <CurrentTime />
+        </View>
+      </View>
     </StyledPrimaryContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+  },
+  topContainer: {
+    paddingTop: 24
+  }
+})
